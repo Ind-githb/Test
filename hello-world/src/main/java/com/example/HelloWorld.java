56a5b413e023db7c23da5fb6d75be534f3998e67
Ind-githb <myul2023@gmail.com>
2024-12-28 21:31:53 +0100

    test sonarqube

--- hello-world/src/main/java/com/example/HelloWorld.java
+++ hello-world/src/main/java/com/example/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
         return "Hello, World!";
     }
 
+    public String getPassword() {
+        return "12345";
+    }
+
     public static void main(String[] args) {
         HelloWorld helloWorld = new HelloWorld();
         System.out.println(helloWorld.sayHello());
